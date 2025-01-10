4328766a94c6853ca788a5f9aefd6d1530c9c1c5
chinmay768 <chinmaysahani07@gmail.com>
2025-01-10 22:51:08 +0530

    Min Falling Path Sum Tabulation

--- stiver/MinimumFallingPathSum.java
+++ stiver/MinimumFallingPathSum.java
@@ -25,9 +25,70 @@ public class MinimumFallingPathSum {
         return Math.min(leftDiag, Math.min(up, rightDiag));
     }
 
+    public static int minFallingPathSumRecursionDP(int[][] matrix) {
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                dp[i][j] = -1;
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < matrix[0].length; i++){
+            min = Math.min(min, minFallingPathSumRecursionHelperDP(matrix, matrix.length - 1, i, dp));
+        }
+
+        return min;
+    }
+
+    public static int minFallingPathSumRecursionHelperDP(int[][] matrix, int m, int n, int[][] dp){
+        if(m == 0) return matrix[m][n];
+
+        if(dp[m][n] != -1) return dp[m][n];
+
+        int leftDiag = Integer.MAX_VALUE;
+        if(n > 0) leftDiag = matrix[m][n] + minFallingPathSumRecursionHelperDP(matrix, m - 1, n - 1, dp);
+
+        int up = matrix[m][n] + minFallingPathSumRecursionHelperDP(matrix, m - 1, n, dp);
+
+        int rightDiag = Integer.MAX_VALUE;
+        if(n < matrix[m].length - 1) rightDiag = matrix[m][n] + minFallingPathSumRecursionHelperDP(matrix, m - 1, n + 1, dp);
+
+        return dp[m][n] = Math.min(leftDiag, Math.min(up, rightDiag));
+    }
+
+    public static int minFallingPathSumTabulation(int[][] matrix){
+        int[][] dp = new int[matrix.length][matrix[0].length];
+
+        for(int i = 0; i < matrix[0].length; i++){
+            dp[0][i] = matrix[0][i];
+        }
+
+        for(int i = 1; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                int leftDiag = Integer.MAX_VALUE;
+                if(j > 0) leftDiag = matrix[i][j] + dp[i - 1][j - 1];
+
+                int up = matrix[i][j] + dp[i - 1][j];
+
+                int rightDiag = Integer.MAX_VALUE;
+                if(j < matrix[i].length - 1) rightDiag = matrix[i][j] + dp[i - 1][j + 1];
+
+                dp[i][j] = Math.min(leftDiag, Math.min(up, rightDiag));
+            }
+        }
+
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i < matrix[matrix.length - 1].length; i++){
+            res = Math.min(res, dp[dp.length - 1][i]);
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         int[][] matrix = {{2,1,3},{6,5,4},{7,8,9}};
 
-        System.out.println(minFallingPathSumRecursion(matrix));
+        System.out.println(minFallingPathSumTabulation(matrix));
     }
 }
