be34249946d06c663d4b943af0a0e2b314de1d3b
chinmay768 <chinmaysahani07@gmail.com>
2025-01-14 21:54:35 +0530

    Subset Sum Equals To Target Tabulatoin

--- stiver/SubsetSumEqualsToTarget.java
+++ stiver/SubsetSumEqualsToTarget.java
@@ -43,10 +43,38 @@ public class SubsetSumEqualsToTarget {
         return incl || excl;
     }
 
+    public static Boolean subsetSumEqualsToTargetRecursionTabulation(int[] arr, int target){
+        Boolean[][] dp = new Boolean[arr.length][target + 1];
+        for(int i = 0; i < dp.length; i++){
+            for (int j = 0; j < dp[0].length; j++){
+                dp[i][j] = false;
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            dp[i][0] = true;
+        }
+
+        dp[0][arr[0]] = true; // If idx is 0 then only the element present at idx 0 is marked as true
+
+        for(int i = 1; i < arr.length; i++){
+            for(int tgt = 1; tgt <= target; tgt++){
+                boolean excl = dp[i - 1][tgt];
+
+                boolean incl = false;
+                if(arr[i] <= tgt) incl = dp[i - 1][tgt - arr[i]];
+
+                dp[i][tgt] = excl || incl;
+            }
+        }
+        return dp[arr.length - 1][target];
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 1, 5, 3, 9, 3};
-        int target = 8;
+        int target = 15;
 
-        System.out.println(subsetSumEqualsToTargetRecursionDP(arr, arr.length - 1, target));
+//        System.out.println(subsetSumEqualsToTargetRecursionDP(arr, arr.length - 1, target));
+        System.out.println(subsetSumEqualsToTargetRecursionTabulation(arr, target));
     }
 }
