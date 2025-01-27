357d5bad18498901ab5478a75a58c50909db57fb
chinmay768 <chinmaysahani07@gmail.com>
2025-01-27 22:19:04 +0530

    Longest Common Subsequence

--- stiver/LongestCommonSubsequence.java
+++ stiver/LongestCommonSubsequence.java
@@ -26,6 +26,8 @@ public class LongestCommonSubsequence {
         }
     }
 
+    //TC O(N * M)
+    //SC O(N * M) + O(N + M)
     public static int longestCommonSubsequenceRecursionDP(String str1, String str2){
         int[][] dp = new int[str1.length()][str2.length()];
         for (int i = 0; i < dp.length; i++){
@@ -53,14 +55,31 @@ public class LongestCommonSubsequence {
         }
     }
 
-//    public static int longestCommonSubsequenceRecursionTabulation(String text1, String text2){
-//
-//    }
+    public static int longestCommonSubsequenceRecursionTabulation(String text1, String text2){
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+
+        for(int i = 0; i < dp.length; i++){
+            dp[i][0] = 0;
+        }
+
+        for (int j = 0; j < dp[0].length; j++){
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= text1.length(); i++){
+            for(int j = 1; j <= text2.length(); j++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[text1.length()][text2.length()];
+    }
 
     public static void main(String[] args) {
-        String str1 = "abcde";
-        String str2 = "ace";
+        String str1 = "bl";
+        String str2 = "yby";
 
-        System.out.println(longestCommonSubsequenceRecursionDP(str1, str2));
+        System.out.println(longestCommonSubsequenceRecursionTabulation(str1, str2));
     }
 }
