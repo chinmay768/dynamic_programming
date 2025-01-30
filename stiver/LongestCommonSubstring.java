ac8b3230e6375d023ea748a4f75b3bbd98e56461
chinmay768 <chinmaysahani07@gmail.com>
2025-01-30 23:52:22 +0530

    LCS

--- stiver/LongestCommonSubstring.java
+++ stiver/LongestCommonSubstring.java
@@ -23,6 +23,34 @@ public class LongestCommonSubstring {
         return Math.max(matchCount, Math.max(exclI, excclJ));
     }
 
+    public static int longestCommonSubstringRecursionDP(String str1, String str2){
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        for(int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return longestCommonSubstringRecursionHelperDP(str1, str2, str1.length() - 1, str2.length() - 1 , 0, dp);
+    }
+
+    // This type of dp won't work because if you use index then idx i j will be previously visited but with diff  lenSoFar thus won't consider recomputing
+    // the max len again for those idx
+//    public static int longestCommonSubstringRecursionHelperDP(String str1, String str2, int i, int j, int lenSoFar, int[][] dp){
+//        if(i < 0 || j < 0) return lenSoFar;
+//
+//        if(dp[i][j] != -1) return dp[i][j];
+//
+//        int matchCount = lenSoFar;
+//        if(str1.charAt(i) == str2.charAt(j)){
+//            // We are not using the traditional way of 1 + lds(i - 1) because we don't know that whether the result is contigous or not
+//            matchCount = longestCommonSubstringRecursionHelperDP(str1, str2, i - 1, j - 1, 1 + lenSoFar, dp);
+//        }
+//
+//        // Reset the counter because you've encountered a mismatch
+//        int exclI = longestCommonSubstringRecursionHelperDP(str1, str2, i - 1, j, 0, dp);
+//        int exclJ = longestCommonSubstringRecursionHelperDP(str1, str2, i, j - 1, 0, dp);
+//
+//        return dp[i][j] = Math.max(matchCount, Math.max(exclI, exclJ));
+//    }
+
     public static int longestCommonSubstringTabulation(String str1, String str2){
         int[][] dp = new int[str1.length() + 1][str2.length() + 1];
 
@@ -57,6 +85,6 @@ public class LongestCommonSubstring {
         String str1 = "tsi";
         String str2 = "sit";
 
-        System.out.println(longestCommonSubstringRecursion(str1, str2));
+        System.out.println(longestCommonSubstringRecursionDP(str1, str2));
     }
 }
