9022d4720785fdeee590eac4b6b52786c9ecf030
chinmay768 <chinmaysahani07@gmail.com>
2025-02-15 23:49:59 +0530

    Buy And Sell stock 4 Tabulation

--- stiver/BuyAndSellStocks4.java
+++ stiver/BuyAndSellStocks4.java
@@ -53,10 +53,50 @@ public class BuyAndSellStocks4 {
         }
     }
 
+    public static int maxProfitTabulation(int[] prices, int k){
+        int[][][] dp = new int[prices.length + 1][2][k + 1]; // we are taking len + 1 bc base case is going till len
+
+        // This is also not necessary as by default its initialized to 0
+        // For cap == 0 index and buy can be anything
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                dp[i][j][0] = 0;
+            }
+        }
+
+        // This is also not necessary as by default its initialized to 0
+        // For index == n buy and cap can be anything
+        for(int i = 0; i < dp[0].length; i++){
+            for(int j = 0; j < dp[0][0].length; j++){
+                dp[prices.length][i][j] = 0;
+            }
+        }
+
+
+        for(int idx = prices.length - 1; idx >= 0; idx--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= k; cap++) { // We are starting from one because 0 was done in base case
+                    if(buy == 1){
+                        dp[idx][buy][cap] = Math.max(
+                                -prices[idx] + dp[idx + 1][0][cap],
+                                dp[idx + 1][1][cap]
+                        );
+                    }else {
+                        dp[idx][buy][cap] = Math.max(
+                                prices[idx] + dp[idx + 1][1][cap - 1],
+                                dp[idx + 1][0][cap]
+                        );
+                    }
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
+
     public static void main(String[] args) {
         int[] prices = {3,2,6,5,0,3};
         int cap = 2;
 
-        System.out.println(maxProfitRecursionDP(prices, cap));
+        System.out.println(maxProfitTabulation(prices, cap));
     }
 }
